c6edbf11de9984ac7435e0d9540d7d61a4a45635
jimolonely <jimolonely@yahoo.com>
2020-08-20 08:23:46 +0800

    chapter05:buffer write find

--- chapter05/src/main/java/com/jimo/bt/ByteBufDemo.java
+++ chapter05/src/main/java/com/jimo/bt/ByteBufDemo.java
@@ -1,10 +1,13 @@
 package com.jimo.bt;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufProcessor;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.util.ByteProcessor;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 import static com.jimo.util.Output.print;
 
@@ -20,6 +23,8 @@ public class ByteBufDemo {
      *
      */
     private final ByteBuf BYTE_FROM_SOMEWHERE = Unpooled.buffer(1024);
+
+    private Random random = new Random();
 
     /**
      * main
@@ -80,6 +85,67 @@ public class ByteBufDemo {
             print("buf:{}", buf.toString());
         }
     }
+
+    /**
+     * 访问里面的数据
+     */
+    public void byteBufCompositeArray() {
+        final CompositeByteBuf compBuf = Unpooled.compositeBuffer();
+        final int length = compBuf.readableBytes();
+        final byte[] array = new byte[length];
+        compBuf.getBytes(compBuf.readerIndex(), array);
+        handleArray(array, 0, array.length);
+    }
+
+    /**
+     * 随机访问索引
+     */
+    public void byteBufAccessRelative() {
+        final ByteBuf buffer = BYTE_FROM_SOMEWHERE;
+        for (int i = 0; i < buffer.capacity(); i++) {
+            final byte b = buffer.getByte(i);
+            print("b={}", (char) b);
+        }
+    }
+
+    /**
+     * 读取所有数据
+     */
+    public void readAllData() {
+        final ByteBuf buffer = BYTE_FROM_SOMEWHERE;
+        while (buffer.isReadable()) {
+            print("byte:{}", buffer.readByte());
+        }
+    }
+
+    /**
+     * 写数据
+     */
+    public void write() {
+        final ByteBuf buffer = BYTE_FROM_SOMEWHERE;
+        while (buffer.writableBytes() >= 4) {
+            buffer.writeInt(random.nextInt());
+        }
+    }
+
+    /**
+     * 使用ByteProcessor来查找
+     */
+    public void byteProcess() {
+        final ByteBuf buffer = BYTE_FROM_SOMEWHERE;
+        // 寻找 \r
+        int index = buffer.forEachByte(ByteProcessor.FIND_CR);
+    }
+
+    /**
+     * 使用ByteBufProcessor来查找\r
+     */
+    @Deprecated
+    private void byteBufProcessor() {
+        final ByteBuf buffer = BYTE_FROM_SOMEWHERE;
+        buffer.forEachByte(ByteBufProcessor.FIND_CR);
+    }
+
 
     /**
      * 做一些处理
